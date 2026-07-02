5d601f018f67ef80e4867a87ece6b2f968011385
Shruthii02 <shruthi.murali02@gmail.com>
2026-07-02 09:28:40 +0530

    Added Donor food api

---
+++ food/serializers.py
@@ -0,0 +1,69 @@
+from rest_framework import serializers
+
+from location.serializers import LocationSerializer
+from location.models import Location
+
+from .models import FoodListing
+
+
+class FoodListingSerializer(serializers.ModelSerializer):
+    location = LocationSerializer()
+    donor_username = serializers.CharField(source='donor.username', read_only=True)
+    donor_id = serializers.IntegerField(source='donor.id', read_only=True)
+
+    class Meta:
+        model = FoodListing
+        fields = (
+            'id',
+            'donor_id',
+            'donor_username',
+            'location',
+            'organization_event_name',
+            'food_name',
+            'quantity',
+            'quantity_unit',
+            'expiry_hours',
+            'expires_at',
+            'other_details',
+            'status',
+            'created_at',
+            'updated_at',
+        )
+        read_only_fields = (
+            'id',
+            'donor_id',
+            'donor_username',
+            'expires_at',
+            'status',
+            'created_at',
+            'updated_at',
+        )
+
+    def validate_quantity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError('Quantity must be greater than 0.')
+        return value
+
+    def validate_expiry_hours(self, value):
+        if value <= 0:
+            raise serializers.ValidationError('Expiry hours must be greater than 0.')
+        return value
+
+    def create(self, validated_data):
+        location_data = validated_data.pop('location')
+        location = Location.objects.create(**location_data)
+        validated_data['location'] = location
+        validated_data['donor'] = self.context['request'].user
+        return FoodListing.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        location_data = validated_data.pop('location', None)
+        if location_data:
+            for attr, value in location_data.items():
+                setattr(instance.location, attr, value)
+            instance.location.save()
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
--- food/views.py
+++ food/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import FoodListing
+from .permissions import IsDonor, IsDonorOwner
+from .serializers import FoodListingSerializer
+
+
+class FoodListingCreateView(generics.CreateAPIView):
+    serializer_class = FoodListingSerializer
+    permission_classes = [IsDonor]
+
+
+class FoodListingMineView(generics.ListAPIView):
+    serializer_class = FoodListingSerializer
+    permission_classes = [IsDonor]
+
+    def get_queryset(self):
+        return FoodListing.objects.filter(donor=self.request.user).select_related(
+            'location', 'donor'
+        )
+
+
+class FoodListingDetailView(generics.RetrieveUpdateAPIView):
+    serializer_class = FoodListingSerializer
+    permission_classes = [IsAuthenticated, IsDonorOwner]
+
+    def get_queryset(self):
+        return FoodListing.objects.filter(donor=self.request.user).select_related(
+            'location', 'donor'
+        )
